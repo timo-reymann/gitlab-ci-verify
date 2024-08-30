a63ee80a92ec3bceffccf9177d0958caf4c9c5a1
Timo Reymann <timo.reymann@deepl.com>
2024-08-31 01:08:12 +0200

    chore: Add shellcheck version test

--- internal/shellcheck/main_test.go
+++ internal/shellcheck/main_test.go
@@ -1,6 +1,9 @@
 package shellcheck
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 func TestValidScript(t *testing.T) {
 	sc, err := NewShellChecker()
@@ -30,4 +33,16 @@ func TestInValidScript(t *testing.T) {
 	if res.ExitCode != 1 {
 		t.Fatalf("Exptected failure exit code but got %d", res.ExitCode)
 	}
+}
+
+func TestShellChecker_Version(t *testing.T) {
+	sc, err := NewShellChecker()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if match, _ := regexp.Match("[0-9]*\\.[0-9]*\\.[0-9]*", []byte(sc.Version())); !match {
+		t.Fatalf("Expted version %s to match semantic release number", sc.Version())
+	}
+
 }
