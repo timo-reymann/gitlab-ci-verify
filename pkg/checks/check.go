c3a9d8e8f059541cb25d7ec242e87d45b0ffc4a7
Timo Reymann <timo.reymann@deepl.com>
2024-09-04 23:08:27 +0200

    feat: Lift newCiYamlMock to proper method

--- pkg/checks/check.go
+++ pkg/checks/check.go
@@ -1,6 +1,7 @@
 package checks
 
 import (
+	format_conversion "github.com/timo-reymann/gitlab-ci-verify/internal/format-conversion"
 	"github.com/timo-reymann/gitlab-ci-verify/pkg/cli"
 	"gopkg.in/yaml.v3"
 )
@@ -14,6 +15,26 @@ type CiYaml struct {
 type CheckInput struct {
 	CiYaml        *CiYaml
 	Configuration *cli.Configuration
+}
+
+// NewCiYaml from byte contents
+func NewCiYaml(content []byte) (*CiYaml, error) {
+	doc, err := format_conversion.ParseYamlNode(content)
+	if err != nil {
+		return nil, err
+	}
+
+	var parsed map[string]any
+	err = doc.Decode(&parsed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CiYaml{
+		FileContent:   content,
+		ParsedYamlMap: parsed,
+		ParsedYamlDoc: doc,
+	}, nil
 }
 
 // Check that runs verifications
