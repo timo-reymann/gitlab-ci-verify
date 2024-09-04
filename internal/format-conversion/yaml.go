7bb3604d2023bb8b8235c5357326c670e4d30c7a
Timo Reymann <timo.reymann@deepl.com>
2024-09-04 23:15:58 +0200

    chore: Remove unused yaml parse method

--- internal/format-conversion/yaml.go
+++ internal/format-conversion/yaml.go
@@ -4,13 +4,6 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// ParseYaml from the given raw content into a generic map
-func ParseYaml(content []byte) (map[string]any, error) {
-	result := map[string]any{}
-	err := yaml.Unmarshal(content, result)
-	return result, err
-}
-
 // ParseYamlNode from the given raw content
 func ParseYamlNode(content []byte) (*yaml.Node, error) {
 	var result yaml.Node
