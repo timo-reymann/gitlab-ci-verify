10f1c375f13d01e79feb28ffd2f9754a805910c4
Timo Reymann <timo.reymann@deepl.com>
2024-10-14 20:53:34 +0200

    feat: Add token source abstraction

---
+++ pkg/gitlab/api/token_source.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"fmt"
+	"github.com/timo-reymann/gitlab-ci-verify/internal/netrc"
+	"github.com/timo-reymann/gitlab-ci-verify/pkg/vault"
+	"os"
+	"strings"
+)
+
+type TokenSource interface {
+	Lookup(baseUrl TokenSourceLookupHints) (string, error)
+}
+
+type TokenSourceLookupHints struct {
+	ExistingToken string
+	BaseUrl       string
+}
+
+type EnvVarTokenSource struct{}
+
+func (e EnvVarTokenSource) Lookup(_ TokenSourceLookupHints) (string, error) {
+	return os.Getenv("GITLAB_TOKEN"), nil
+}
+
+type NetRcTokenSource struct{}
+
+func (n NetRcTokenSource) Lookup(t TokenSourceLookupHints) (string, error) {
+	userNetrc, err := netrc.ReadUserNetrc()
+	if err != nil {
+		return "", err
+	}
+
+	credentials, err := netrc.GetCredentials(userNetrc, t.BaseUrl)
+	if err != nil {
+		return "", err
+	}
+
+	return credentials.Password, nil
+}
+
+type VaultTokenSource struct{}
+
+func (v VaultTokenSource) Lookup(t TokenSourceLookupHints) (string, error) {
+	if !strings.HasPrefix(t.ExistingToken, "vault://") && !strings.ContainsRune(t.ExistingToken, '#') {
+		return "", nil
+	}
+
+	spec := t.ExistingToken[8:len(t.ExistingToken)]
+	specParts := strings.SplitN(spec, "#", 2)
+	if len(specParts) != 2 {
+		return "", fmt.Errorf("malformed spec '%s', should be in format 'vault://<field>#<path>'", t.ExistingToken)
+	}
+
+	return vault.GetSecretWithDefaultConfiguration(specParts[0], specParts[1])
+}
