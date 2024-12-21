339b281ae483430c273212f76f88b3a04f3e2587
Timo Reymann <mail@timo-reymann.de>
2024-12-21 12:24:12 +0100

    feat: Add more details for failed lint requests

--- pkg/gitlab/ci-yaml/detect.go
+++ pkg/gitlab/ci-yaml/detect.go
@@ -21,11 +21,16 @@ type VerificationResultWithRemoteInfo struct {
 	LintResult *api.CiLintResult
 }
 
+type validationResult struct {
+	verificationResult VerificationResultWithRemoteInfo
+	occurredErrs       []error
+}
+
 // GetFirstValidationResult starts a parallel request to all remotes and tries to use the API to lint the CI file
 // the first to report a result will be used as a validation result. If none of the remotes can produce a result or
 // timeout is reached, the validation is aborted and the error is set accordingly.
 func GetFirstValidationResult(remoteInfos []git.GitlabRemoteUrlInfo, token string, baseUrlOverwrite string, ciYaml []byte, timeout time.Duration) (*VerificationResultWithRemoteInfo, error) {
-	result := make(chan VerificationResultWithRemoteInfo, 1)
+	result := make(chan validationResult, 1)
 	ctx, cancel := context.WithCancel(context.Background())
 	for _, remoteInfo := range remoteInfos {
 		go func(r *git.GitlabRemoteUrlInfo) {
@@ -36,11 +41,12 @@ func GetFirstValidationResult(remoteInfos []git.GitlabRemoteUrlInfo, token strin
 				baseUrl = r.Hostname
 			}
 
+			var occurredErrs []error
 			gl := api.NewClientWithMultiTokenSources(baseUrl, token)
 			lintRes, err := gl.LintCiYaml(ctx, r.RepoSlug, ciYaml)
 			if err != nil {
+				occurredErrs = append(occurredErrs, err)
 				logging.Warn("lint request failed for remote", remoteInfo.Hostname, ":", err.Error())
-				return
 			}
 
 			// ignore sending on the closed channel
@@ -48,32 +54,40 @@ func GetFirstValidationResult(remoteInfos []git.GitlabRemoteUrlInfo, token strin
 				recover()
 			}()
 
-			result <- VerificationResultWithRemoteInfo{
-				RemoteInfo: r,
-				LintResult: lintRes,
+			result <- validationResult{
+				verificationResult: VerificationResultWithRemoteInfo{
+					RemoteInfo: r,
+					LintResult: lintRes,
+				},
+				occurredErrs: occurredErrs,
 			}
 			logging.Verbose("lint request succeeded for remote", remoteInfo.Hostname, ": valid =", lintRes.Valid)
-
 		}(&remoteInfo)
 	}
-
 	var err error
 
-	// wait for the first validation to arrive, or timeout
-	var res VerificationResultWithRemoteInfo
+	// wait for validations to arrive, or timeout
 	select {
-	case res = <-result:
-		break
+	case res := <-result:
+		// when there are errors -> save them, in case no validation is possible
+		if res.verificationResult.LintResult == nil || len(res.occurredErrs) > 0 {
+			errs := append(res.occurredErrs, ErrNoResult)
+			err = errors.Join(errs...)
+			break
+		}
+
+		// if there are no errors doing request validation did its thing
+		// cancel waiting for the other requests and return the lint result
+		cancel()
+		return &res.verificationResult, nil
+
 	case <-time.After(timeout):
 		err = ErrTimeout
+		break
 	}
 
 	// cancel context either way
 	cancel()
 
-	if res.LintResult == nil {
-		return nil, ErrNoResult
-	}
-
-	return &res, err
+	return nil, err
 }
