26665ea907bf4bf6e03f5c53f524408edb30cde8
Timo Reymann <mail@timo-reymann.de>
2024-12-22 11:26:00 +0100

    feat: Add yaml path utils to get line numbers from yamlpath query

---
+++ internal/yamlpathutils/line_numbers.go
@@ -0,0 +1,30 @@
+package yamlpathutils
+
+import (
+	"github.com/vmware-labs/yaml-jsonpath/pkg/yamlpath"
+	"gopkg.in/yaml.v3"
+)
+
+// PathToLineNumbers runs the path on the given node and returns all node line numbers that resolved from the query
+func PathToLineNumbers(node *yaml.Node, path *yamlpath.Path) []int {
+	nodes, err := path.Find(node)
+	if err != nil {
+		return []int{}
+	}
+
+	lines := make([]int, len(nodes))
+	for idx, n := range nodes {
+		lines[idx] = n.Line - 1
+	}
+
+	return lines
+}
+
+// PathToFirstLineNumber runs the path on the given node and returns the first line number that resolved from the query
+func PathToFirstLineNumber(node *yaml.Node, path *yamlpath.Path) int {
+	lines := PathToLineNumbers(node, path)
+	if len(lines) > 0 {
+		return lines[0]
+	}
+	return -1
+}
