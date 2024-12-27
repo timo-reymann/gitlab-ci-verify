8b4d872aaaeb43f955e4edc1d85278d42d01270f
Timo Reymann <timo.reymann@deepl.com>
2024-12-27 14:06:55 +0100

    feat: Add more verbose rego logging

--- pkg/checks/rego_bundle_check.go
+++ pkg/checks/rego_bundle_check.go
@@ -2,6 +2,7 @@ package checks
 
 import (
 	"fmt"
+	"github.com/timo-reymann/gitlab-ci-verify/internal/logging"
 	"github.com/timo-reymann/gitlab-ci-verify/internal/rego_policies"
 )
 
@@ -16,12 +17,14 @@ func (r BundleCheck) convertToCheckFinding(raw map[string]any) (*CheckFinding, e
 func (r BundleCheck) Run(i *CheckInput) ([]CheckFinding, error) {
 	rpm := rego_policies.NewRegoPolicyManager()
 
+	logging.Verbose("Load rego bundle", r.BundlePath)
 	if err := rpm.LoadBundle(r.BundlePath); err != nil {
 		return nil, fmt.Errorf("failed to load rego bundle %s: %s", r.BundlePath, err)
 	}
 
 	results, err := queryManagerForFindings(rpm, i)
 	if err != nil {
+		logging.Verbose("Failed to query rego bundle", r.BundlePath, err)
 		return nil, err
 	}
 
