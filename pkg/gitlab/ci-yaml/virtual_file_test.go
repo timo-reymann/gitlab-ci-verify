d1cc99b5cff4fb28e4fa72a4609949a44283b572
Timo Reymann <mail@timo-reymann.de>
2025-03-14 22:01:19 +0100

    feat: Add resolve for virtual file

--- pkg/gitlab/ci-yaml/virtual_file_test.go
+++ pkg/gitlab/ci-yaml/virtual_file_test.go
@@ -1,7 +1,9 @@
 package ci_yaml
 
 import (
+	"github.com/timo-reymann/gitlab-ci-verify/pkg/location"
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -45,5 +47,68 @@ func TestCreateVirtualCiYamlFile(t *testing.T) {
 		if part.Path != expectedParts[i] {
 			t.Errorf("Expected part path to be %s, got %s", expectedParts[i], part.Path)
 		}
+	}
+}
+
+func TestVirtualCiYamlFile_Resolve(t *testing.T) {
+	tests := []struct {
+		name        string
+		parts       []*VirtualCiYamlFilePart
+		line        int
+		expected    *VirtualCiYamlFilePart
+		expectedLoc *location.Location
+	}{
+		{
+			name: "line in first part",
+			parts: []*VirtualCiYamlFilePart{
+				{StartLine: 1, EndLine: 10, Path: "part1.yml"},
+				{StartLine: 11, EndLine: 20, Path: "part2.yml"},
+			},
+			line:        5,
+			expected:    &VirtualCiYamlFilePart{StartLine: 1, EndLine: 10, Path: "part1.yml"},
+			expectedLoc: location.NewLocation("part1.yml", 4),
+		},
+		{
+			name: "line in second part",
+			parts: []*VirtualCiYamlFilePart{
+				{StartLine: 1, EndLine: 10, Path: "part1.yml"},
+				{StartLine: 11, EndLine: 20, Path: "part2.yml"},
+			},
+			line:        15,
+			expected:    &VirtualCiYamlFilePart{StartLine: 11, EndLine: 20, Path: "part2.yml"},
+			expectedLoc: location.NewLocation("part2.yml", 4),
+		},
+		{
+			name: "line before first part",
+			parts: []*VirtualCiYamlFilePart{
+				{StartLine: 1, EndLine: 10, Path: "part1.yml"},
+				{StartLine: 11, EndLine: 20, Path: "part2.yml"},
+			},
+			line:        0,
+			expected:    nil,
+			expectedLoc: nil,
+		},
+		{
+			name: "line after last part",
+			parts: []*VirtualCiYamlFilePart{
+				{StartLine: 1, EndLine: 10, Path: "part1.yml"},
+				{StartLine: 11, EndLine: 20, Path: "part2.yml"},
+			},
+			line:        21,
+			expected:    nil,
+			expectedLoc: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &VirtualCiYamlFile{
+				Parts: tt.parts,
+			}
+			result, loc := v.Resolve(tt.line)
+			if !reflect.DeepEqual(tt.expected, result) || !reflect.DeepEqual(tt.expectedLoc, loc) {
+				t.Errorf("Resolve(%d) = %v, %v; want %v, %v", tt.line, result, loc, tt.expected, tt.expectedLoc)
+			}
+		})
 	}
 }
