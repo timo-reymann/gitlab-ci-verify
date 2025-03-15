b012a259eeafee77eb1e6c88a8f700a0b6ee97b9
Timo Reymann <mail@timo-reymann.de>
2025-03-15 19:51:52 +0100

    refactor: Use app level logging for http logger

--- internal/httputils/retryable_client.go
+++ internal/httputils/retryable_client.go
@@ -2,9 +2,28 @@ package httputils
 
 import (
 	"github.com/hashicorp/go-retryablehttp"
+	"github.com/timo-reymann/gitlab-ci-verify/internal/logging"
 	"net/http"
 	"time"
 )
+
+type httpLogger struct{}
+
+func (h httpLogger) Error(msg string, keysAndValues ...interface{}) {
+	logging.Debug(msg, keysAndValues)
+}
+
+func (h httpLogger) Info(msg string, keysAndValues ...interface{}) {
+	logging.Verbose(msg, keysAndValues)
+}
+
+func (h httpLogger) Debug(msg string, keysAndValues ...interface{}) {
+	logging.Debug(msg, keysAndValues)
+}
+
+func (h httpLogger) Warn(msg string, keysAndValues ...interface{}) {
+	logging.Warn(msg, keysAndValues)
+}
 
 // NewRetryableClient creates a new http client with
 // retryablehttp.Client as transport
@@ -12,5 +31,6 @@ func NewRetryableClient() *http.Client {
 	retryHttpClient := retryablehttp.NewClient()
 	retryHttpClient.RetryMax = 3
 	retryHttpClient.HTTPClient.Timeout = 5 * time.Second
+	retryHttpClient.Logger = &httpLogger{}
 	return retryHttpClient.StandardClient()
 }
