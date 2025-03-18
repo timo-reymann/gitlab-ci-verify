3cd292c9064f127242f81815caa73be9086675cf
Timo Reymann <mail@timo-reymann.de>
2025-03-18 20:13:48 +0100

    refactor: Use object for validation result input method

--- pkg/gitlab/ci-yaml/detect.go
+++ pkg/gitlab/ci-yaml/detect.go
@@ -26,24 +26,32 @@ type validationResult struct {
 	occurredErrs       []error
 }
 
+type ValidationResultInput struct {
+	RemoteInfos      []git.GitlabRemoteUrlInfo
+	Token            string
+	BaseUrlOverwrite string
+	CiYaml           []byte
+	Timeout          time.Duration
+}
+
 // GetFirstValidationResult starts a parallel request to all remotes and tries to use the API to lint the CI file
 // the first to report a result will be used as a validation result. If none of the remotes can produce a result or
 // timeout is reached, the validation is aborted and the error is set accordingly.
-func GetFirstValidationResult(remoteInfos []git.GitlabRemoteUrlInfo, token string, baseUrlOverwrite string, ciYaml []byte, timeout time.Duration) (*VerificationResultWithRemoteInfo, error) {
+func GetFirstValidationResult(vri *ValidationResultInput) (*VerificationResultWithRemoteInfo, error) {
 	result := make(chan validationResult, 1)
 	ctx, cancel := context.WithCancel(context.Background())
-	for _, remoteInfo := range remoteInfos {
+	for _, remoteInfo := range vri.RemoteInfos {
 		go func(r *git.GitlabRemoteUrlInfo) {
 			var baseUrl string
-			if baseUrlOverwrite != "" {
-				baseUrl = baseUrlOverwrite
+			if vri.BaseUrlOverwrite != "" {
+				baseUrl = vri.BaseUrlOverwrite
 			} else {
 				baseUrl = r.Hostname
 			}
 
 			var occurredErrs []error
-			gl := api.NewClientWithMultiTokenSources(baseUrl, token)
-			lintRes, err := gl.LintCiYaml(ctx, r.RepoSlug, ciYaml)
+			gl := api.NewClientWithMultiTokenSources(baseUrl, vri.Token)
+			lintRes, err := gl.LintCiYaml(ctx, r.RepoSlug, vri.CiYaml)
 			if err != nil {
 				occurredErrs = append(occurredErrs, err)
 				logging.Warn("lint request failed for remote", remoteInfo.Hostname, ":", err.Error())
@@ -81,7 +89,7 @@ func GetFirstValidationResult(remoteInfos []git.GitlabRemoteUrlInfo, token strin
 		cancel()
 		return &res.verificationResult, nil
 
-	case <-time.After(timeout):
+	case <-time.After(vri.Timeout):
 		err = ErrTimeout
 		break
 	}
